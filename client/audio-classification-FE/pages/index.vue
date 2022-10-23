2f4ded76cb3c1e5842ad0c297bb19386e6217d39
Martin Sir <sirmart2@fit.cvut.cz>
2022-10-23 15:52:56 +0200

    Created nuxt project

---
+++ client/audio-classification-FE/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from 'vue'
+
+// variables
+
+// functions
+
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="text-center">
+      <span>Audio classification</span>
+    </div>
+
+    <div class="text-center">
+      <span>Input</span>
+    </div>
+
+    <div class="text-center">
+      <span>Result</span>
+    </div>
+  </div>
+</template>
