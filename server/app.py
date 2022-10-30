b6807e4df071be99dad667c0031291a36d383ef6
Marketa Minarova <minarma5@fit.cvut.cz>
2022-10-30 21:09:39 +0100

    update app.py

--- server/app.py
+++ server/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import request
-import pandas as pd
-import librosa
+import audio_classification
 
 app = Flask(__name__)
 
 
 @app.post("/")
 def genre_post():
-    # funkce na podobnostni vyhledavani
+    # get audio file from request
     audio = request.args.get('audio')
+
+    # compute distances of audio from genres
+    dist = audio_classification.classify(audio)
+
+    # normalize distances to 1
+
     return "<h1>No genre, yet!</h1>"
 
