3c84abcc1b165635906070843b3f86752b03aa84
Marketa Minarova <minarma5@fit.cvut.cz>
2022-11-01 16:20:23 +0100

    update main file

--- server/audio_classification.py
+++ server/audio_classification.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import librosa
-from dtw import *
 import os
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+import numpy as np
 
 
 #  classical, jazz, pop, metal, rock, reggae, country, disco
@@ -12,24 +14,28 @@ def load_genres(genre):
     return l
 
 
+def dtw_distance(arg1, arg2):
+    distance, path = fastdtw(arg1, arg2, dist=euclidean)
+    return distance
+
+
 def extract_features(audio):
     y, sr = librosa.load(audio)
-    return librosa.feature.mfcc(y=y, sr=sr)
+    return librosa.feature.mfcc(y=y, sr=sr).T
 
 
 def compute_dist(genre_files, audio):
     dist = 0
     for x in genre_files:
-        dist += dtw(x, audio)
-        print(type(dist))
+        dist += dtw_distance(x, audio)
     return dist / len(genre_files)
 
 
 def classify(audio):
     classical = load_genres("classical")
-    country = load_genres("country")
-    disco = load_genres("disco")
-    hiphop = load_genres("hiphop")
+    # country = load_genres("country")
+    # disco = load_genres("disco")
+    # hiphop = load_genres("hiphop")
     # jazz = load_genres("jazz")
     # metal = load_genres("metal")
     # pop = load_genres("pop")
@@ -40,7 +46,7 @@ def classify(audio):
 
     distances = []
     # genres = [classical, country, disco, hiphop, jazz, metal, pop, reggae, rock]
-    genres = [classical, country, disco, hiphop]
+    genres = [classical]
 
     for x in genres:
         distances.append(compute_dist(x, au_mfcc))
