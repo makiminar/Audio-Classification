ae0e501a89c2d5a3867320ea4231359b6a219c97
Marketa Minarova <minarma5@fit.cvut.cz>
2022-11-11 19:05:08 +0100

    add normalization

--- server/audio_classification.py
+++ server/audio_classification.py
@@ -7,22 +7,30 @@ import numpy as np
 #  classical, jazz, pop, metal, rock, reggae, country, disco
 def load_genres(genre):
     l = []
-    for file in os.listdir('./data/' + genre):
+    data_dir = os.listdir('./data/' + genre)
+    for file in data_dir[:10]:
         l.append(extract_features('./data/' + genre + "/" + str(file)))
     return l
+
+
+def normalize(dist):
+    norm_dist = []
+    sm = np.array(dist).sum()
+
+    for x in dist:
+        norm_dist.append(np.round((x/sm)*100))
+    return norm_dist
 
 
 def euclidean(arg1, arg2):
     sm = 0
     for x in range(len(arg1)):
-        sm += (arg1[x] - arg2[x])**2
+        sm += (arg1[x] - arg2[x]) ** 2
     return np.sqrt(sm)
 
 
-# dtw for 2 sequences
 def dtw_distance(arg1, arg2, window):
     n, m = arg1.shape[1], arg2.shape[1]
-
     w = np.max([window, abs(n - m)])
 
     # initialize DTW array
@@ -36,7 +44,7 @@ def dtw_distance(arg1, arg2, window):
 
     for i in range(1, n + 1):
         for j in range(np.max([1, i - w]), np.min([m, i + w]) + 1):
-            cost = euclidean(arg1[:, i-1], arg2[:, j-1])
+            cost = euclidean(arg1[:, i - 1], arg2[:, j - 1])
             DTW[i, j] = cost + np.min([DTW[i - 1, j], DTW[i, j - 1], DTW[i - 1, j - 1]])
 
     return DTW[n, m]
@@ -50,28 +58,29 @@ def extract_features(audio):
 def compute_dist(genre_files, audio):
     dist = 0
     for x in genre_files:
-        dist += dtw_distance(x, audio, 3)
+        dist += (dtw_distance(x, audio, 5))
     return dist / len(genre_files)
 
 
 def classify(audio):
     classical = load_genres("classical")
-    # country = load_genres("country")
-    # disco = load_genres("disco")
-    # hiphop = load_genres("hiphop")
-    # jazz = load_genres("jazz")
-    # metal = load_genres("metal")
-    # pop = load_genres("pop")
-    # reggae = load_genres("reggae")
-    # rock = load_genres("rock")
+    country = load_genres("country")
+    disco = load_genres("disco")
+    hiphop = load_genres("hiphop")
+    jazz = load_genres("jazz")
+    metal = load_genres("metal")
+    pop = load_genres("pop")
+    reggae = load_genres("reggae")
+    rock = load_genres("rock")
 
     au_mfcc = extract_features(audio)
 
     distances = []
-    # genres = [classical, country, disco, hiphop, jazz, metal, pop, reggae, rock]
-    genres = [classical]
+    genres = [classical, country, disco, hiphop, jazz, metal, pop, reggae, rock]
 
-    # for x in genres:
-    #   distances.append(compute_dist(x, au_mfcc))
+    for x in genres:
+        w = compute_dist(x, au_mfcc)
+        print(w)
+        distances.append((1/w))
 
-    return distances
+    return normalize(distances)
